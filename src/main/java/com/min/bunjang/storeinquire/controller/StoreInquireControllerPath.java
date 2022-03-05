f6117552b61560a13444a9299fdf5a6c93d0dfa3
mins1031 <urisegea@naver.com>
2022-03-05 15:58:51 +0900

    feat : 상점문의 http파일 테스트

--- src/main/java/com/min/bunjang/storeinquire/controller/StoreInquireControllerPath.java
+++ src/main/java/com/min/bunjang/storeinquire/controller/StoreInquireControllerPath.java
@@ -1,6 +1,6 @@
 package com.min.bunjang.storeinquire.controller;
 
 public class StoreInquireControllerPath {
-    public static final String CREATE_INQUIRY = "/store-inquiry";
-    public static final String DELETE_INQUIRY = "/store-inquiry/{inquireNum}";
+    public static final String CREATE_INQUIRY = "/store-inquire";
+    public static final String DELETE_INQUIRY = "/store-inquire/{inquireNum}";
 }
