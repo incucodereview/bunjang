ef6fbc63a83fc1596a0798da919e912ce99e5e44
mins1031 <urisegea@naver.com>
2022-03-05 15:47:44 +0900

    feat : 상점 http파일 테스트

--- src/main/java/com/min/bunjang/store/controller/StoreControllerPath.java
+++ src/main/java/com/min/bunjang/store/controller/StoreControllerPath.java
@@ -4,5 +4,5 @@ public class StoreControllerPath {
     public static final String STORE_CREATE = "/store";
     public static final String STORE_INTRODUCE_CONTENT_UPDATE = "/store/introduce";
     public static final String STORE_NAME_UPDATE = "/store/name";
-    public static final String STORE_PLUS_VISITOR = "/store/visitor";
+    public static final String STORE_PLUS_VISITOR = "/store-visitor";
 }
