5f07b4fa5c3c584e73c4cf64f1828d3cd8e0a070
mins1031 <urisegea@naver.com>
2022-03-10 22:14:11 +0900

    style : 충돌 해결

--- src/main/java/com/min/bunjang/product/model/Product.java
+++ src/main/java/com/min/bunjang/product/model/Product.java
@@ -8,9 +8,12 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
