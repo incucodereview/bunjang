ef45bfcbd07f45d21be84ba9cc52715207e15a74
mins1031 <urisegea@naver.com>
2022-05-18 11:24:21 +0900

    feat: 검색로직 인덱스 추가

--- src/main/java/com/min/bunjang/product/model/Product.java
+++ src/main/java/com/min/bunjang/product/model/Product.java
@@ -18,9 +18,11 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -31,6 +33,10 @@ import java.util.Set;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+        @Index(name = "idx_product_name_trade_location", columnList = "productName, tradeLocation"),
+        @Index(name = "idx_trade_location", columnList = "tradeLocation")
+})
 public class Product extends BasicEntity {
 
     @NotBlank
